c0a4dace1d12fbb10eaeec7d3b13005cfc309a34
LeonardoBonacci <aabcemhu@mailfence.com>
2022-05-15 10:00:18 +1200

    run account-cdc local and docker

--- account-cdc/src/main/java/guru/bonacci/heroes/accountcdc/KafkaTopicConfig.java
+++ account-cdc/src/main/java/guru/bonacci/heroes/accountcdc/KafkaTopicConfig.java
@@ -4,11 +4,13 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 
 import guru.bonacci.heroes.kafka.KafkaTopicNames;
 
+@Profile("!kube")
 @Configuration
 @EnableKafka
 public class KafkaTopicConfig {
